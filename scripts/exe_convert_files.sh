8e510fd237ea878d05fa865cae20f26125a621a0
Dmitriev Egor Olegovich <bearlune@master.unicluster.campus.mephi.ru>
2018-07-19 17:07:00 +0300

    added folders

---
+++ scripts/exe_convert_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Convert file $1 to tree $2
+
+	cd /home/verbarius/VorticityHic
+	./convert -i $1 -o $2
---
+++ scripts/exe_gen_plots.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Draw plots for tree $1 and put to tree $2
+
+        cd /home/verbarius/VorticityHic
+	./main -i $1 -o $2
